5dea4af8f002873711325972203d4ac6b8cfe2d6
John Matthews <jmatthews@redhat.com>
2012-12-03 18:55:39 -0500

    Got rid of "extends App"

--- src/main/scala/ListNewsgroups.scala
+++ src/main/scala/ListNewsgroups.scala
@@ -3,7 +3,12 @@ import scala.util.Properties
 import org.apache.commons.net.nntp.NNTPClient;
 import org.apache.commons.net.nntp.NewsgroupInfo;
 
-object Main extends App {
+object Main {
+  val hostname = "news.newshosting.com"
+  val port = 119  // Remember, non-SSL for now
+  val username = Properties.envOrElse("USERNAME", "")
+  val password = Properties.envOrElse("PASSWORD", "")
+  
   def getClient(hostname: String, port: Int, username: String, password: String): NNTPClient = {
     val client = new NNTPClient()
     client.connect(hostname, port)
@@ -15,14 +20,12 @@ object Main extends App {
 
   def getNewsgroups(client: NNTPClient) = Option(client.listNewsgroups())
 
-  val hostname = "news.newshosting.com"
-  val port = 119  // Remember, non-SSL for now
-  val username = Properties.envOrElse("USERNAME", "")
-  val password = Properties.envOrElse("PASSWORD", "")
-  val client = getClient(hostname, port, username, password)
-  getNewsgroups(client) match {
+  def main(args: Array[String]) {
+    val client = getClient(hostname, port, username, password)
+    getNewsgroups(client) match {
       case Some(x: Array[NewsgroupInfo]) => println(x.length + " newsgroups found")
       case None => println("Error with client connection.  Check ${USERNAME} and ${PASSWORD}")
+    }
   }
 }
 
